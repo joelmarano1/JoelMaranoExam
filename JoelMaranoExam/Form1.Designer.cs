d7c0b74d4a32aac61ada032ee68bb59d55083310
joelmarano1 <joelmarano1@outlook.com>
2021-02-16 22:06:53 +0800

    Add project files.

---
+++ JoelMaranoExam/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace JoelMaranoExam
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 30);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(191, 23);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(228, 30);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(191, 23);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(13, 59);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(83, 30);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtResult.Location = new System.Drawing.Point(13, 114);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(703, 218);
+            this.txtResult.TabIndex = 3;
+            this.txtResult.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 337);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.RichTextBox txtResult;
+    }
+}
+
---
+++ JoelMaranoExam/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+
+namespace JoelMaranoExam
+{
+    public partial class Form1 : Form
+    {
+        string mycontent;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            //if (email != "" && password != "")
+            //{
+               GetRequest();
+                txtResult.Text = mycontent;
+            //}
+        }
+
+        async  void GetRequest(string url= "" )
+        {
+            //IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>()
+            //{
+
+            //    new KeyValuePair<string, string>("login", txtEmail.Text),
+            //    new KeyValuePair<string, string>("password", txtPassword.Text)
+
+
+            //};
+            List<KeyValuePair<string, string>> values = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("login", txtEmail.Text),
+                new KeyValuePair<string, string>("password", txtPassword.Text)
+            };
+
+            var json = JsonConvert.SerializeObject(values);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            //HttpContent postContent = new FormUrlEncodedContent(queries);
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                  client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", "c1461646f53e0d15e1fd9d6cf5e2be4306b08982");
+
+                //using (HttpResponseMessage responsex = await client.GetAsync("https://github.com/session"))
+                //{
+
+                //}
+
+                using (HttpResponseMessage response = await client.PostAsync("https://api.github.com/authorizations", data))
+               {
+                   using (HttpContent content = response.Content)
+                   {
+                    mycontent = content.ReadAsStringAsync().Result;
+
+
+                    }
+                }
+            }
+        }
+    }
+}
